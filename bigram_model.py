1f6696fa4ab71758f94096cfb3c5b328b025e99f
elrrowwe <248607@edu.p.lodz.pl>
2023-10-29 17:35:22 +0100

    good accuracy achieved, cuda issues fixed

--- bigram_model.py
+++ bigram_model.py
@@ -10,38 +10,38 @@ from sklearn.model_selection import train_test_split
 # temporarily added THE HAUNTED MAN AND THE GHOST’S BARGAIN to the oliver_twist data set
 
 # defining some hyperparameters
-batch_size = 64  # how many sequences (sentences of some length) are processed at once
-block_size = 256  # how long the aforementioned sequences are (context length)
+batch_size = 16  # how many sequences (sentences of some length) are processed at once
+block_size = 32  # how long the aforementioned sequences are (context length)
 train_iters = 5000
-eval_interval = 500
-lr = 0.001
+eval_interval = 100
+lr = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 eval_iters = 200
-n_embed = 384  # the number of embedding dimensions (the dimensionality of embedding vectors)
-n_head = 6  # the number of heads in the multi-head attention layer
+n_embed = 64  # the number of embedding dimensions (the dimensionality of embedding vectors)
+n_head = 4  # the number of heads in the multi-head attention layer
 n_layer = 6
-dropout = 0.17  # the dropout rate
+dropout = 0.0  # the dropout rate
 
-# oliver_twist = open('pg730.txt', 'r', encoding='utf8').read()
-shakespeare = open('/kaggle/input/tiny-shakespeare/tiny_shakespeare.txt', 'r', encoding='utf8').read()
+# oliver_twist = open('pg73'0.txt', 'r', encoding='utf8').read()
+shakespeare = open('tiny_shakespeare.txt', 'r', encoding='utf8').read()
 
 # getting all the characters, like in the n-gram model
 chars = sorted(list(set(shakespeare)))
 vocab_size = len(chars)
 
 # a simple (de-)tokenizer
-stoi = {s: i for i, s in enumerate(chars)}
-itos = {i: s for s, i in stoi.items()}
+stoi = {s: i for i,s in enumerate(chars)}
+itos = {i: s for i,s in enumerate(chars)}
 
 encode = lambda s: [stoi[ch] for ch in s]  # tokenize some characters
-decode = lambda i: ' '.join([itos[num] for num in i])  # detokenize some integers
+decode = lambda i: ''.join([itos[num] for num in i])  # detokenize some integers
 
 # tokenizing the entire data set
 enc = torch.tensor(encode(shakespeare), dtype=torch.long, device=device)
 
 # splitting the text into train, test portions
-train, test = train_test_split(enc, test_size=0.3)
+train, test = train_test_split(enc, shuffle=False, test_size=0.1)
 
 
 def get_batch(split):
@@ -49,7 +49,8 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,), device=device)
     x = torch.stack([data[i:i + block_size] for i in ix])
     y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
-
+    
+    x, y = x.to(device), y.to(device)
     return x, y
 
 
@@ -77,14 +78,16 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embed, head_size, bias=False).to(device)
         self.value = nn.Linear(n_embed, head_size, bias=False).to(device)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)).to(device))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x)
         q = self.query(x)
-        wei = q @ k.transpose(-2, -1) * C**(-0.5)
+        wei = q @ k.transpose(-2, -1) * (C**-0.5)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
-        wei = F.softmax(wei, dim=1)
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
         v = self.value(x)
         out = wei @ v
 
@@ -94,13 +97,14 @@ class Head(nn.Module):
 class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
-        self.heads = nn.ModuleList(Head(head_size) for _ in range(num_heads))
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embed, n_embed).to(device)
         self.dropout = nn.Dropout(dropout).to(device)
 
     def forward(self, x):
         out = torch.cat([head(x) for head in self.heads], dim=-1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
+        
         return out
 
 
@@ -122,13 +126,13 @@ class Block(nn.Module):
     def __init__(self, n_embed, n_head):
         super().__init__()
         head_size = n_embed // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
+        self.attn = MultiHeadAttention(n_head, head_size)
         self.ffn = Dense(n_embed)
         self.ln1 = nn.LayerNorm(n_embed).to(device)
         self.ln2 = nn.LayerNorm(n_embed).to(device)
 
     def forward(self, x):
-        x = x + self.sa(self.ln1(x))
+        x = x + self.attn(self.ln1(x))
         x = x + self.ffn(self.ln2(x))
 
         return x
@@ -140,12 +144,7 @@ class BigramModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed).to(device)
         self.position_embedding_table = nn.Embedding(block_size, n_embed).to(device)
         self.lm_head = nn.Linear(n_embed, vocab_size).to(device)
-        self.sa_heads = MultiHeadAttention(num_heads=n_head, head_size=int(n_embed/n_head)).to(device)
-        self.blocks = nn.Sequential(
-            Block(n_embed, n_head=n_head).to(device),
-            Block(n_embed, n_head=n_head).to(device),
-            Block(n_embed, n_head=n_head).to(device))
-        self.ffn = Dense(n_embed).to(device)
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head=n_head) for _ in range(n_layer)])
         self.ln_f = nn.LayerNorm(n_embed).to(device)
 
     def forward(self, idx, targets=None):
@@ -153,9 +152,9 @@ class BigramModel(nn.Module):
         tok_embeddings = self.token_embedding_table(idx)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
         x = tok_embeddings + pos_emb
-        x = self.sa_heads(x)
-        logits = self.lm_head(x)
         x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
 
         if targets is None:
             loss = None
@@ -181,7 +180,7 @@ class BigramModel(nn.Module):
 
 bi = BigramModel().to(device)
 
-optimizer = torch.optim.Adam(bi.parameters(), lr=lr)
+optimizer = torch.optim.AdamW(bi.parameters(), lr=lr)
 
 for i in range(train_iters):
 
@@ -197,4 +196,4 @@ for i in range(train_iters):
 
 idx = torch.zeros((1, 1), dtype=torch.long, device=device)
 
-print(decode(bi.generate(idx, max_new_tokens=300)[0].tolist()))
+print(decode(bi.generate(idx, max_new_tokens=2000)[0].tolist()))
